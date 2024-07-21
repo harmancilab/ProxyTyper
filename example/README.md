ef8bd17eca71fbd6bc69a0908b996f364a9639b3
Arif Harmanci <arif@harmancilab.org>
2024-07-21 11:53:03 -0500

    Code Overhaul

--- example/README.md
+++
@@ -1,3 +0,0 @@
-# ProxyTyper: Proxy-panel Generation for Outsourcing Genotype Imputation 
-
-This folder contains examples of proxytyping a panel and an example imputation pipeline.
--- src/prxytypr_x_chisqr.h
+++
@@ -1,9 +0,0 @@
-#ifndef __X_CHISQR__
-#define __X_CHISQR__
-
-double approx_gamma(double Z);
-double x_gamma(double N);
-//static double igf(double S, double Z);
-double chisqr(int Dof, double Cv);
-
-#endif // __X_CHISQR__
